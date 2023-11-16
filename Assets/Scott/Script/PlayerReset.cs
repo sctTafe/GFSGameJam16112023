cd1dedc74c62be20567b3ded979f0869e75fa5db
Scott B <94416375+sctTafe@users.noreply.github.com>
2023-11-16 23:35:30 +1000

    Player Prefab Reset Point Done

--- Assets/Scott/Script/PlayerReset.cs
+++ Assets/Scott/Script/PlayerReset.cs
@@ -6,13 +6,21 @@ public class PlayerReset : MonoBehaviour
 {
     public Transform _spawnPoint;
     public float _playerRespawnYHeight = 0f;
-    private Transform _playerTransform;
 
+
+    private Transform _playerTransform;
+    private PlayerController _playerController;
+    private Vector3 _resetPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        _resetPosition = _spawnPoint.position;
+        _playerController = GetComponent<PlayerController>();
         _playerTransform = this.transform;
+        if (_playerTransform == null) {
+            _resetPosition = Vector3.zero;
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +28,12 @@ public class PlayerReset : MonoBehaviour
     {
         if (_playerTransform.position.y <= _playerRespawnYHeight) 
         {
-            _playerTransform.position = _spawnPoint.position;
+            _playerTransform.position = _resetPosition;
         }
+    }
+
+    void ZeroRBMomentum()
+    {
+        _playerController.fn_ZeroRigidBodyMomentum();
     }
 }
