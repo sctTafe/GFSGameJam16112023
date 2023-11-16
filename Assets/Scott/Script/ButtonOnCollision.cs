2366767abb9e0a5a4008f2c164e202c294d9fadb
Scott B <94416375+sctTafe@users.noreply.github.com>
2023-11-16 17:10:55 +1000

    Button Working

--- Assets/Scott/Script/ButtonOnCollision.cs
+++ Assets/Scott/Script/ButtonOnCollision.cs
@@ -1,15 +1,35 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ButtonOn : MonoBehaviour
 {
     public UnityEvent _OnCollision;
-    public string _spannerTag;
+    public string _spannerTag = "Spanner";
+    private bool _isWaiting = false;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == _spannerTag)
         {
-            _OnCollision?.Invoke();
+            if (!_isWaiting)
+            {
+                Debug.Log("Spanner Collision Hit");
+                _OnCollision?.Invoke();
+
+                _isWaiting = true;
+                StartCoroutine(Wait());
+            }
+           
         }
     }
+
+    IEnumerator Wait()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+        _isWaiting = false;
+
+
+    }
+
 }
