3cbb73fad932d0d815a4992eb79136e2761f1fda
LargeKnome <44670186+LargeKnome@users.noreply.github.com>
2023-11-19 01:04:07 +1000

    Made first level

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,41 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Laser : MonoBehaviour
 {
     public bool disabled = false;
-    public LineRenderer lr;
-    public GameObject pos1;
-    public GameObject pos2;
+
+    public LaserGate[] gates;
+    
     // Start is called before the first frame update
 
+    [Serializable] public struct LaserGate
+    {
+        public GameObject pos1;
+        public GameObject pos2;
+        public LineRenderer line;
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if (disabled)
         {
-            lr.gameObject.SetActive(false);
-            pos1.SetActive(false);
-            pos2.SetActive(false);
+            foreach (LaserGate gate in gates)
+            {
+                gate.line.gameObject.SetActive(false);
+                gate.pos1.SetActive(false);
+                gate.pos2.SetActive(false);
+            }
             return;
         }
-        lr.gameObject.SetActive(true);
-        pos1.SetActive(true);
-        pos2.SetActive(true);
-
-        lr.SetPosition(0, lr.transform.InverseTransformPoint(pos1.transform.position) + Random.insideUnitSphere*0.01f);
-        lr.SetPosition(1, lr.transform.InverseTransformPoint(pos2.transform.position) + Random.insideUnitSphere*0.01f);
-
-        RaycastHit hit;
-        Vector3 dir = Vector3.Normalize(pos2.transform.position - pos1.transform.position);
-        if (Physics.Raycast(pos1.transform.position, dir, out hit))
+        
+        foreach (LaserGate gate in gates)
         {
-            if (hit.transform.CompareTag("Player"))
+            gate.line.gameObject.SetActive(true);
+            gate.pos1.SetActive(true);
+            gate.pos2.SetActive(true);
+
+            Vector3 gatePos1 = gate.pos1.transform.position;
+            Vector3 gatePos2 = gate.pos2.transform.position;
+            
+            gate.line.SetPosition(0, gate.line.transform.InverseTransformPoint(gatePos1) + UnityEngine.Random.insideUnitSphere*0.01f);
+            gate.line.SetPosition(1, gate.line.transform.InverseTransformPoint(gatePos2) + UnityEngine.Random.insideUnitSphere*0.01f);
+            
+            RaycastHit hit;
+            Vector3 dir = Vector3.Normalize(gatePos2-gatePos1);
+            
+            if (Physics.Raycast(gatePos1, dir, out hit))
             {
-                GameManager.instance.ResetLevel();
+                if (hit.transform.CompareTag("Player"))
+                {
+                    GameManager.instance.ResetLevel();
+                }
             }
         }
+
     }
     public void fn_ToggleLaserOn()
     {
