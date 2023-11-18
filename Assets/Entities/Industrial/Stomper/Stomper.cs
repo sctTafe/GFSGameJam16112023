ceea19d90e08d7005876d88b5f98efecb21e7115
LargeKnome <44670186+LargeKnome@users.noreply.github.com>
2023-11-19 09:45:42 +1000

    fixed probuilder and tomscene

--- Assets/Entities/Industrial/Stomper/Stomper.cs
+++ Assets/Entities/Industrial/Stomper/Stomper.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Stomper : MonoBehaviour
 {
+    public float waitTime = 0f;
     public float offset = 0f;
     public bool disabled = false;
     public Animation stomp;
+
+    public GameObject StomperObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +26,20 @@ public class Stomper : MonoBehaviour
             {
                 stomp.Play();
             }
-            yield return new WaitForSeconds(stomp.clip.length + 0.1f);
+            yield return new WaitForSeconds(stomp.clip.length + waitTime +0.1f);
         }
     }
 
     public void fn_ToggleStomperOn()
     {
         disabled = !disabled;
+    }
+
+    public void OnCollisionStay(Collision other)
+    {
+        if (other.transform.CompareTag("Player") && Vector3.Distance(StomperObject.transform.position, transform.position) < 2)
+        {
+            Debug.Log("Dead");
+        }
     }
 }
